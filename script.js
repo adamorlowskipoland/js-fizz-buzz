81ec3382d27870ae9ee69e2c8eb6d5bb30fa6747
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-04-23 19:23:28 +0200

    add counting display

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const model = {
     "btnFizz" : document.getElementById('btn-fizz'),
     "btnBuzz" : document.getElementById('btn-buzz'),
     "btnFizzBuzz" : document.getElementById('btn-fizzbuzz'),
+    "counterDisplay" : document.getElementById('counter'),
 }
 
 const operator = {
@@ -10,13 +11,36 @@ const operator = {
         return model.btnStart;
     },
     "getBtnFizz" : function() {
-
+        return model.btnFizz;
     },
     "getBtnBuzz" : function() {
-        
+        return model.btnBuzz;      
     },
     "getBtnFizzBuzz" : function() {
-        
+        return model.btnFizzBuzz;        
+    },
+    "getCounterDisplay" : function() {
+        return model.counterDisplay;        
+    },
+    "counter" : 0,
+    "interval" : 0,
+
+    "startInterval" : function() {
+        this.counter = 0;
+        clearInterval(this.interval);
+        this.interval = setInterval(function() {
+            if (operator.counter < 100) {
+                operator.startCounting();
+                view.showCounter();
+            } else {
+                clearInterval(operator.interval);
+            }
+        }, 100);
+    },
+
+    "startCounting" : function() {
+        operator.counter++;
+        console.log(operator.counter);
     }
 }
 
@@ -24,9 +48,25 @@ const view = {
     "initDisplay" : function() {
         this.eventListeners();
     },
+    "showCounter" : function() {
+        const counter = operator.getCounterDisplay();
+        counter.textContent = operator.counter;
+    },
     "eventListeners" : function() {
         const btnStart = operator.getBtnStart();
         btnStart.addEventListener('click', function(){
+            operator.startInterval();       
+        });
+        const btnFizz = operator.getBtnFizz();
+        btnFizz.addEventListener('click', function(){
+            console.log(this);        
+        });
+        const btnBuzz = operator.getBtnBuzz();
+        btnBuzz.addEventListener('click', function(){
+            console.log(this);        
+        });
+        const btnFizzBuzz = operator.getBtnFizzBuzz();
+        btnFizzBuzz.addEventListener('click', function(){
             console.log(this);        
         });
     }
